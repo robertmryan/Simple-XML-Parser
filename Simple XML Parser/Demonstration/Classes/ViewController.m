4882ce7cc723250dfd629b504e27f5c521c07589
robertmryan <robert.ryan@mindspring.com>
2013-05-28 19:59:54 -0400

    Use custom `UITableViewCell` subclass, `Cell`; add image cache and asynchronous updating of images; parse `description` field for `img src` tags.

--- Simple XML Parser/Demonstration/Classes/ViewController.m
+++ Simple XML Parser/Demonstration/Classes/ViewController.m
@@ -9,10 +9,12 @@
 #import "ViewController.h"
 #import "Parser.h"
 #import "WebViewController.h"
+#import "Cell.h"
 
 @interface ViewController ()
 
 @property (nonatomic, strong) NSArray *objects;
+@property (nonatomic, strong) NSCache *imageCache;
 
 @end
 
@@ -21,6 +23,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+
+    self.imageCache = [[NSCache alloc] init];
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
        
@@ -56,6 +60,27 @@
     }
 }
 
+- (NSArray *)imagesFromString:(NSString *)string
+{
+    NSMutableArray *imageUrls = [NSMutableArray array];
+    
+    NSError *error = NULL;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"(<img\\s[\\s\\S]*?src\\s*?=\\s*?['\"](.*?)['\"][\\s\\S]*?>)+?"
+                                                                           options:NSRegularExpressionCaseInsensitive
+                                                                             error:&error];
+
+    [regex enumerateMatchesInString:string
+                            options:0
+                              range:NSMakeRange(0, [string length])
+                         usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
+
+                             NSString *imgUrlString = [string substringWithRange:[result rangeAtIndex:2]];
+                             [imageUrls addObject:[NSURL URLWithString:imgUrlString]];
+                         }];
+
+    return imageUrls;
+}
+
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
@@ -71,13 +96,53 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     static NSString *cellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
+    Cell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
     
     NSDictionary *object = _objects[indexPath.row];
-    cell.textLabel.text = object[@"title"];
-    cell.detailTextLabel.text = object[@"pubDate"];
-    
+    cell.cellTitle.text = object[@"title"];
+    cell.cellSubtitle.text = object[@"pubDate"];
+
+    cell.cellImageView.image = nil;
+    [self tableView:tableView updateImageForCell:cell indexPath:indexPath];
     return cell;
 }
 
+// It's probably better to use a `UIImageView` category, such as provided by `SDWebImage` or
+// `AFNetworking`, which does all of this, but I was reluctant to add third party libraries
+// to this project which is focusing on XML Parsing.
+
+- (void)tableView:(UITableView *)tableView updateImageForCell:(Cell *)cell indexPath:(NSIndexPath *)indexPath
+{
+    NSDictionary *object = _objects[indexPath.row];
+
+    NSArray *imageUrls = [self imagesFromString:object[@"description"]];
+
+    if ([imageUrls count] > 0)
+    {
+        NSURL *url = imageUrls[0];
+        UIImage *image = [self.imageCache objectForKey:[url absoluteString]];
+        if (image)
+        {
+            cell.cellImageView.image = image;
+        }
+        else
+        {
+            dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                NSData *data = [NSData dataWithContentsOfURL:imageUrls[0]];
+                UIImage *image = [UIImage imageWithData:data];
+                if (!image) return;
+
+                [self.imageCache setObject:image forKey:[url absoluteString]];
+
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    Cell *updateCell = (id)[tableView cellForRowAtIndexPath:indexPath];
+                    if (updateCell)
+                    {
+                        updateCell.cellImageView.image = image;
+                    }
+                });
+            });
+        }
+    }
+}
 @end
